4232d4d21c4694866ac04f3e569167158707c167
KARUPPASAMY M <mkaruppas477@gmail.com>
2025-11-21 14:40:34 +0530

    vercel to deployed

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,4 @@
 import axios from "axios";
 export default axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://peer-project-emc.vercel.app/"
+  baseURL: import.meta.env.VITE_API_URL || "https://peer-project-emc.vercel.app"
 });
